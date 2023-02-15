c3232ec130289caead62157f018d8a9cebb00fa0
FazliddinFayziev <fazrez4515@gmail.com>
2023-02-15 00:46:07 -0800

    secret key

--- client/script.js
+++ client/script.js
@@ -87,8 +87,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('https://codex-uzb.onrender.com/', {
-    mode: 'no-cors',
+  const response = await fetch('http://localhost:5000/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
